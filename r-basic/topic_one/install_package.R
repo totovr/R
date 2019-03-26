ee6c82792c9d58ba3b689b4a5dfbcb6736741887
Antonio Vega <a.vegaramirez76@gmail.com>
2019-03-26 15:22:48 +0900

    load library

---
+++ r-basic/topic_one/install_package.R
@@ -0,0 +1,11 @@
+# install.packages("name_package", dep = TRUE)
+
+install.packages("tidyverse", dep = TRUE)
+
+library(tidyverse)
+
+install.packages("magic", dep = TRUE)
+
+library(magic)
+
+magic(6)
